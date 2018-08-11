e5a508819a452bac7e7e2badf7a41e0b510ed1c1
Willian Ribeiro Corrêa <willianribeirocorrea@gmail.com>
2018-08-10 22:55:16 -0300

    Initial commit

---
+++ ufabcsnack/src/main/java/com/ufabc/ufabcsnack/UfabcsnackApplication.java
@@ -0,0 +1,12 @@
+package com.ufabc.ufabcsnack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfabcsnackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfabcsnackApplication.class, args);
+	}
+}
---
+++ ufabcsnack/src/test/java/com/ufabc/ufabcsnack/UfabcsnackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ufabc.ufabcsnack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UfabcsnackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
